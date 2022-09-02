16943161c9218059d6af4578e1cd062a3ceada99
Nico Pom <niconp7@yahoo.fr>
2022-09-02 18:51:35 +0200

    Start Component / Css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Start from "./components/Start";
 
 export default function App() {
+  const [start, setStart] = React.useState(true);
+
+  function startQuiz() {
+    setStart(true);
+  }
+
   return (
-    <div>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main className="app--container">
+      {!start ? <Start startQuiz={startQuiz} /> : <Quiz />}
+    </main>
   );
 }
