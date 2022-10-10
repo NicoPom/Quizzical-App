50bce681fc69b947e6a16778245b07aa0be09b2b
Nico Pom <niconp7@yahoo.fr>
2022-10-10 12:06:46 +0200

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
     if (start) {
       async function getQuizData() {
         try {
-          const response = await fetch("https://opentdb.com/api.php?amount=3");
+          const response = await fetch("https://opentdb.com/api.php?amount=5");
           const data = await response.json();
           const quizData = data.results.map((data) => {
             const answers = [
